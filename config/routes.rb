a3dcf86be80a66bc526686c2077644bb0ced09fa
schbrongx <thomas.pesendorfer@gmail.com>
2016-09-30 13:23:31 +0200

    devise: allow accepting invitations

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,11 @@ Rails.application.routes.draw do
   resources :certificates
   resources :keystores
   resources :settings, :except => :show
-  devise_for :users
+  devise_for :users, :skip => [:registrations]                                          
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'            
+  end
   
 
   # The priority is based upon order of creation: first created -> highest priority.
