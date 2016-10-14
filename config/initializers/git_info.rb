8367c2eba143f7bb2ce024b08dd093096c705e08
schbrongx <thomas.pesendorfer@gmail.com>
2016-10-14 10:45:13 +0200

    improving git info in dev environment on index#index

--- config/initializers/git_info.rb
+++ config/initializers/git_info.rb
@@ -1,3 +1,2 @@
-GIT_BRANCH = `git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3`
-GIT_COMMIT = `git log --pretty=format:'%h' -n 1`
+GIT_COMMIT = `git log --pretty=oneline -n1`
 GIT_COMMIT_TIMESTAMP = `git log --pretty=format:'%ai' -n 1`
