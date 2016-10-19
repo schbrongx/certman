b662d640d3621f196a7aeeea73893c62cbcd3016
schbrongx <thomas.pesendorfer@gmail.com>
2016-10-19 10:27:09 +0200

    allowing empty csrtemplate

--- app/models/csr.rb
+++ app/models/csr.rb
@@ -7,6 +7,5 @@ class Csr < ActiveRecord::Base
   validates :name, presence: true, length: { in: 5..100 }, uniqueness: true
   validates :content, presence: true, uniqueness: true
   validates :keypair_id, presence: true
-  validates :csrtemplate_id, presence: true
   validates :cn, presence: true
 end
