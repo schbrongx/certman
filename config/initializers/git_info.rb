eda4e9b5314202d80f6a1ce73131f005965d2f1b
schbrongx <thomas.pesendorfer@gmail.com>
2016-11-02 09:28:34 +0100

    only init git_info in development

--- config/initializers/git_info.rb
+++ config/initializers/git_info.rb
@@ -1,4 +1,5 @@
-puts "  Loading initializers/git_info.rb".green
-
-GIT_COMMIT = `git log --pretty=oneline -n1`
-GIT_COMMIT_TIMESTAMP = `git log --pretty=format:'%ai' -n 1`
+if Rails.env.development?
+  puts "  Loading initializers/git_info.rb".green
+  GIT_COMMIT = `git log --pretty=oneline -n1`
+  GIT_COMMIT_TIMESTAMP = `git log --pretty=format:'%ai' -n 1`
+end  # if
