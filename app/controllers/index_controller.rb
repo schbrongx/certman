4b1efcbe78bf7a015579ce000e620891642dc783
schbrongx <thomas.pesendorfer@gmail.com>
2016-11-03 09:18:45 +0100

    improving display of notifications on index page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,8 +3,8 @@ class IndexController < ApplicationController
   
   def index
     @keystores = Keystore.includes(:keypair, :csr, :certificate)
-    @csrs = Csr.includes(:keypair, :csrtemplate)
-    @certificates = Certificate.includes(:certificatetype, :csr)
+    @csrs = Csr.all
+    @certificates = Certificate.all
     
     # create and zero-out variables for notifications on index#index
     @keystores_missing_items = 0
@@ -16,14 +16,26 @@ class IndexController < ApplicationController
       if (k.keypair.nil? || k.csr.nil? || k.certificate.nil?)
         @keystores_missing_items += 1
       end  # if
-    end  #do
+    end  # do
     
-    # TODO : count unsigned CSRs
+    # TODO : list unsigned CSRs
+    @csrs.each do |c|
+      unless (Certificate.find_by_csr_id(c.id))
+        @csrs_unsigned += 1
+      end  # unless
+    end  # do
     
     # TODO : count soon expiring certificates
+    # TODO : define Setting.expiryWarningDays
+    @expiryWarningDays = 60
+    @certificates.each do |c|
+      logger.debug "  index_controller.rb: @certificates.each, c.id = #{c.id.to_s}".yellow
+      cert = OpenSSL::X509::Certificate.new c.content
+    end  #do
     
     
     # sum up all notifications
     @notifications = @keystores_missing_items +  @csrs_unsigned + @certificates_expiring_soon
+
   end  # def
 end  # class
