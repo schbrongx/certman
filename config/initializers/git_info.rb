eed012218eabb971dc2ee4202f253f0a3369e09e
schbrongx <thomas.pesendorfer@gmail.com>
2016-11-03 09:17:38 +0100

    improving git infos in development environment

--- config/initializers/git_info.rb
+++ config/initializers/git_info.rb
@@ -1,5 +1,4 @@
 if Rails.env.development?
   puts "  Loading initializers/git_info.rb".green
-  GIT_COMMIT = `git log --pretty=oneline -n1`
-  GIT_COMMIT_TIMESTAMP = `git log --pretty=format:'%ai' -n 1`
+  GIT_COMMIT = `git log --stat -n1`
 end  # if
